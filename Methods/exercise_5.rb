4b11f2f1bf6296674f565729ef15654d88a451cd
Daniel Damaschke <thewarblingwhisker@gmail.com>
2016-10-09 13:00:00 -0500

    Adding Methods chapter exercises.

---
+++ Methods/exercise_5.rb
@@ -0,0 +1,8 @@
+def scream(words)
+  words = words + "!!!!"
+  puts words
+end
+
+scream("Yippeee")
+
+# This prints out the words followed by exclamations, but returns 'nil' because the last line is 'puts'.
