765086099e9b41ae2595aaa555f16df09b5eec39
Daniel Damaschke <thewarblingwhisker@gmail.com>
2017-01-11 22:23:52 -0600

    updating rock_paper_scissors.rb

--- Lesson_2/rock_paper_scissors.rb
+++ Lesson_2/rock_paper_scissors.rb
@@ -4,54 +4,73 @@ VALID_CHOICES = { 'r' => 'rock',
                   'l' => 'lizard',
                   'sp' => 'spock' }
 
+WINNING_LIST = { 'rock' => %w(scissors lizard),
+                  'paper' => %w(rock spock),
+                  'scissors' => %w(paper lizard),
+                  'spock' => %w(rock scissors),
+                  'lizard' => %w(spock paper) }
+
 def prompt(message)
   puts "=> #{message}"
 end
 
 def won?(first, second)
-  (first == 'rock' && second == 'scissors') ||
-    (first == 'rock' && second == 'lizard') ||
-    (first == 'paper' && second == 'rock') ||
-    (first == 'paper' && second == 'spock') ||
-    (first == 'scissors' && second == 'paper') ||
-    (first == 'scissors' && second == 'lizard') ||
-    (first == 'spock' && second == 'rock') ||
-    (first == 'spock' && second == 'Scissors') ||
-    (first == 'lizard' && second == 'spock') ||
-    (first == 'lizard' && second == 'paper')
+  WINNING_LIST.fetch(first).include?(second)
+end
+
+def victory(player, computer)
+  if player > computer
+    prompt("You beat the computer!")
+  elsif computer > player
+    prompt("The computer won!")
+  else
+    prompt("It was a tie!")
+  end
 end
 
 def display_results(player, computer)
   if won?(player, computer)
-    prompt("You won!")
+    prompt("You won!\n")
   elsif won?(computer, player)
-    prompt("Computer won!")
+    prompt("Computer won!\n")
   else
-    prompt("It's a tie!")
+    prompt("It's a tie!\n")
   end
 end
 
 loop do
-  choice = ''
-  loop do
-    prompt("Choose one: #{VALID_CHOICES.values.join(', ')}; type 'r', 'p',
-    'sc', 'l', or 'sp'.")
-    choice = gets.chomp
+  player = ''
+  player_score = 0
+  computer_score = 0
+  5.times do
+    loop do
+      prompt("PLAYER SCORE: #{player_score} / COMPUTER SCORE: #{computer_score}")
+      prompt("Choose one: (r) rock, (p) paper, (sc) scissors, 
+        (sp) spock, (l) lizard.\n\n")
+      player = gets.chomp
 
-    if VALID_CHOICES.include?(choice)
-      choice = VALID_CHOICES[choice]
-      break
-    else
-      prompt("That's not a valid choice.")
+      if VALID_CHOICES.include?(player)
+        player = VALID_CHOICES.fetch(player)
+        break
+      else
+        prompt("That's not a valid choice.")
+      end
     end
+
+    computer = VALID_CHOICES.values.sample
+
+    prompt("You chose: #{player}; Computer chose: #{computer}")
+
+    display_results(player, computer)
+    if won?(player, computer)
+      player_score += 1
+    elsif won?(computer, player)
+      computer_score += 1
+    end
+
+    system 'clear'
   end
-
-  computer_choice = VALID_CHOICES.values.sample
-
-  prompt("You chose: #{choice}; Computer chose: #{computer_choice}")
-
-  display_results(choice, computer_choice)
-
+  victory(player_score, computer_score)
   prompt("Do you want to play again?")
   answer = gets.chomp
 
