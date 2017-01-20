db20b6057da13dc8c7277606a97659c38de95d17
Daniel Damaschke <thewarblingwhisker@gmail.com>
2017-01-20 23:24:25 +0000

    Adding mutation to small_problems.

---
+++ exercises_small_problems/Easy_2/mutation.rb
@@ -0,0 +1,8 @@
+array1 = %w(Moe Larry Curly Shemp Harpo Chico Groucho Zeppo)
+array2 = []
+array1.each { |value| array2 << value }
+array1.each { |value| value.upcase! if value.start_with?('C', 'S') }
+puts array2
+
+# this mutates array2 as well, because array2 references the same object as array1,
+# which is modified inside the block.
